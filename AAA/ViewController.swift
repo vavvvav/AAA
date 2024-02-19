f5cab17b6579ac96fbd766e929cf1083ef229c9e
vavvvav <vaskinanas@gmail.com>
2024-02-19 23:08:18 +0300

    SOME APP

--- AAA/ViewController.swift
+++ AAA/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterButton: UIButton!
+    @IBOutlet weak var counterLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    var counterValue: Int = 0
+    func updateCountLabel() {
+        counterLabel.text = "Значение счетчика: \(counterValue)"
+    }
+    
+    @IBAction func addNumber(_ sender: Any) {
+        counterValue += 1
+        updateCountLabel()
+    }
+    
 }
 
